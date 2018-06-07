a6a4fb96647e11a2bcabfd6c968a1a76c9805728
yonatan-ramirez <yonatan.ramirez557@gmail.com>
2018-06-07 16:51:07 -0700

    fighters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+fighter1= {name: 'Jack', power: 54};
+fighter2={name:'Eric', power:56};
+
+ngOnInit(){
+
 }
+}
+
