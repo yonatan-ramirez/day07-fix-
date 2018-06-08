cf91a69c3581fed8301e8340a72f06c0fcb8fae8
yonatan-ramirez <yonatan.ramirez557@gmail.com>
2018-06-08 16:32:32 -0700

    ComponentsRecap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+class User{
+  constructor(public id:number, public name:string){
+
+  }
+}
+class CarBrand{
+
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+  `,
+  styleUrls: ['./app.component.css'],
+  styles:[`
+  h1{
+    margin: auto;
+    color: gray;
+  }
+  `]
 })
 export class AppComponent {
   title = 'app';
+  displayParagraph:boolean = true;
+
+  unknown: undefined;
+  person: string= 'john';
+  num: number =7;
+  stop: boolean= false;
+  people: any;
 }
---
+++ src/app/greet/greet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[greet]',
+  templateUrl: './greet.component.html',
+  styleUrls: ['./greet.component.css']
+})
+export class GreetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
