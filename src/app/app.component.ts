2ca9d042939ebdef0665d6afbd60ac81503b2d1b
yonatan-ramirez <yonatan.ramirez557@gmail.com>
2018-06-08 10:56:45 -0700

    types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  any:any;
+  str: string = 'sharks are awesome!';
+  num: number = 5;
+  space: void;
+
+  chad: null;
+
+  arr: any[] = [4 , 'hello world', true, false];
+  arrTwo:number[]= [4,6];
+  arrThree: Array<number> =[4,5];
+  arrFour:string[] = ['hola', 'mundo'];
+  arrFive: Array<string> = ['das', 'Verdania'];
 }
