bb31b9735f6ceae2b8096dc9cacfae1619e01063
yonatan-ramirez <yonatan.ramirez557@gmail.com>
2018-06-08 14:00:11 -0700

    fighters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+interface fighter{
+  name: string;
+  number1?: number;
+returnNum?():any;
+  
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit{
   title = 'app';
-
-fighter1= {name: 'Jack', power: 54};
-fighter2={name:'Eric', power:56};
+fighter1: fighter= {name:"Mike", number1: 10};
+fighter2: fighter = {name:'goku', returnNum: () => {console.log(34)}};
+// fighter1= {name: 'Jack', power: 54};
+// fighter2={name:'Eric', power:56};
 
 ngOnInit(){
-
+console.log(this.fighter1, this.fighter2, this.fighter2.returnNum);
 }
 }
 
