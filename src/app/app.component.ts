4744e27d0efa7fab8727777f6ab2193a7bd09c6b
yonatan-ramirez <yonatan.ramirez557@gmail.com>
2018-06-11 12:38:34 -0700

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+
   displayParagraph: boolean = true;
+
+
+  arr = [1,2,3, 'str'];
+  users=[{
+    name: 'Jack',
+    age: 30,
+  },{
+    name:'Jill',
+    age: 43,
+  },{
+    name: 'james',
+    age: 21,
+  }];
+
 }
+
